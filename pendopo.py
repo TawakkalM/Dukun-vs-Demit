fe29b8eefcb23e34248adc6ef7f554371fe35871
M.Raihan Athalah Ilham <162966950+Hannntastix@users.noreply.github.com>
2024-05-05 20:59:41 +0700

    Update pendopo.py

--- pendopo.py
+++ pendopo.py
@@ -2,14 +2,8 @@ import pygame
 
 pygame.init()
 
-# Pendopo load gambar
-pendopo_img_100 = pygame.image.load('<--LOKASI GAMBAR--!>').convert_alpha()
-pendopo_img_50 = pygame.image.load('<--LOKASI GAMBAR--!>').convert_alpha()
-pendopo_img_25 = pygame.image.load('<--LOKASI GAMBAR--!>').convert_alpha()
-
-
 class Pendopo():
-    def __init__(self, image100, image50, image25, x, y, scale):
+    def __init__(self, image100, image50, image25, bg, x, y, scale):
         self.kesehatan = 1000
         self.maks_kesehatan = self.kesehatan
         self.ditembak = False
@@ -22,11 +16,18 @@ class Pendopo():
         self.gambar100 = pygame.transform.scale(image100, (int(width * scale), int(height * scale)))
         self.gambar50 = pygame.transform.scale(image50, (int(width * scale), int(height * scale)))
         self.gambar25 = pygame.transform.scale(image25, (int(width * scale), int(height * scale)))
+        self.bg = bg
         self.rect = self.gambar100.get_rect()
         self.rect.x = x
         self.rect.y = y
 
+        self.screen = pygame.display.set_mode((900, 650))
+        pygame.display.set_caption("DVD")
+
     def render(self):
+        # Render latar belakang
+        self.screen.blit(self.bg, (0, 0))
+        
         if self.kesehatan <= 250:
             self.gambar = self.gambar25
         elif self.kesehatan <= 500:
@@ -34,7 +35,7 @@ class Pendopo():
         else:
             self.gambar = self.gambar100
 
-        screen.blit(self.gambar, self.rect)
+        self.screen.blit(self.gambar, self.rect)
 
     def perbaiki(self):
         if self.uang >= 1000 and self.kesehatan < self.maks_kesehatan:
@@ -47,7 +48,3 @@ class Pendopo():
         if self.uang >= 500:
             self.maks_kesehatan += 250
             self.uang -= 500
-
-
-# Buat Pendoponya
-pendopo = Pendopo(pendopo_img_100, pendopo_img_50, pendopo_img_25, SCREEN_WIDTH - 250, SCREEN_HEIGHT - 300, 0.3)
