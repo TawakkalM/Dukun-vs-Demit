56985f8f8617b39fd67e964fffb19fd6ee31b9f6
skatchoww <bntngfkr@gmail.com>
2024-05-19 17:13:49 +0700

    update class dukun

--- dukun.py
+++ dukun.py
@@ -48,14 +48,14 @@ class Dukun1(Dukun):
         x_dist = pos[0] - self.rect[0]
         y_dist = -(pos[1] - self.rect[1])
         self.angle = math.degrees(math.atan2(y_dist, x_dist))
-        # get mouse click
+        # ketika mouse di klik
         if pygame.mouse.get_pressed()[0] and self.fired == False and pos[0] < 720 and pos[1] > 70:
             self.fired = True
             self.image = pygame.transform.scale(self.dukun1_shoot, (self.width, self.height))
             self.display(screen)
             sihir = Serangan(self.gambar_sihir, 685, 380, self.angle)
             group_serangan.add(sihir)
-        # reset mouse click
+        # reset mouse
         if pygame.mouse.get_pressed()[0] == False:
             self.image = pygame.transform.scale(self.gambar_dasar, (self.width, self.height))
             self.display(screen)
@@ -65,7 +65,7 @@ class Dukun1(Dukun):
 class Dukun2(Dukun):
     def __init__(self, gambar, lebar, tinggi, x, y):
         base_sihir = pygame.image.load("assets/dukun/sihir_2.png").convert_alpha()
-        self.dukun1_shoot = pygame.image.load("assets/dukun/dukun_2_attack.png").convert_alpha()
+        self.dukun2_shoot = pygame.image.load("assets/dukun/dukun_2_attack.png").convert_alpha()
         s_w = base_sihir.get_width()
         s_h = base_sihir.get_height()
         self.gambar_sihir = pygame.transform.scale(base_sihir, (int(s_w* 0.080), int(s_h* 0.080)))
@@ -80,14 +80,14 @@ class Dukun2(Dukun):
         x_dist = pos[0] - self.rect[0]
         y_dist = -(pos[1] - self.rect[1])
         self.angle = math.degrees(math.atan2(y_dist, x_dist))
-        # get mouse click
+        # ketika mouse di klik
         if pygame.mouse.get_pressed()[0] and self.fired == False and pos[0] < 720 and pos[1] > 70:
             self.fired = True
-            self.image = pygame.transform.scale(self.dukun1_shoot, (self.width, self.height))
+            self.image = pygame.transform.scale(self.dukun2_shoot, (self.width, self.height))
             self.display(screen)
             sihir = Serangan(self.gambar_sihir, 685, 380, self.angle)
             group_serangan.add(sihir)
-        # reset mouse click
+        # reset mouse
         if pygame.mouse.get_pressed()[0] == False:
             self.image = pygame.transform.scale(self.gambar_dasar, (self.width, self.height))
             self.display(screen)
@@ -96,7 +96,7 @@ class Dukun2(Dukun):
 class Dukun3(Dukun):
     def __init__(self, gambar, lebar, tinggi, x, y):
         base_sihir = pygame.image.load("assets/dukun/sihir_3.png").convert_alpha()
-        self.dukun1_shoot = pygame.image.load("assets/dukun/dukun_3_attack.png").convert_alpha()
+        self.dukun3_shoot = pygame.image.load("assets/dukun/dukun_3_attack.png").convert_alpha()
         s_w = base_sihir.get_width()
         s_h = base_sihir.get_height()
         self.gambar_sihir = pygame.transform.scale(base_sihir, (int(s_w* 0.080), int(s_h* 0.080)))
@@ -111,14 +111,14 @@ class Dukun3(Dukun):
         x_dist = pos[0] - self.rect[0]
         y_dist = -(pos[1] - self.rect[1])
         self.angle = math.degrees(math.atan2(y_dist, x_dist))
-        # get mouse click
+        # ketika mouse di klik
         if pygame.mouse.get_pressed()[0] and self.fired == False and pos[0] < 720 and pos[1] > 70:
             self.fired = True
-            self.image = pygame.transform.scale(self.dukun1_shoot, (self.width, self.height))
+            self.image = pygame.transform.scale(self.dukun3_shoot, (self.width, self.height))
             self.display(screen)
             sihir = Serangan(self.gambar_sihir, 685, 380, self.angle)
             group_serangan.add(sihir)
-        # reset mouse click
+        # reset mouse
         if pygame.mouse.get_pressed()[0] == False:
             self.image = pygame.transform.scale(self.gambar_dasar, (self.width, self.height))
             self.display(screen)
